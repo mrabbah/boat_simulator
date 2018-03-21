5aa8443525a9b47a907f94b23e3693c7537c9684
RABBAH Mahmoud <mrabbah@gmail.com>
2018-03-21 19:47:01 +0000

    Progress saving

---
+++ Assets/Scritps/ThirdPersonCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public float distanceAway;
+    public float distanceUp;
+    public float smooth;
+
+    private Transform follow;
+    private Vector3 targetPosition;
+
+	
+	void Start () {
+        follow = this.transform.parent.gameObject.transform;
+	}
+	
+	
+	void LateUpdate () {
+        if(follow.position.y + 2 > 0) {
+            targetPosition = follow.position + follow.up * distanceUp - follow.forward * distanceAway;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smooth * Time.deltaTime);
+
+            transform.LookAt(follow);
+        } else {
+            targetPosition = new Vector3(follow.position.x + follow.up.x * distanceUp - follow.forward.x * distanceAway, 
+                                         10, 
+                                         follow.position.z + follow.up.z * distanceUp - follow.forward.z * distanceAway);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smooth * Time.deltaTime);
+
+            transform.LookAt(follow);
+        }
+        	
+    }
+}
